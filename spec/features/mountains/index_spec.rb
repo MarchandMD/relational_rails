9a6717020eacd961e185e7cf2b5f735d735099a4
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-11 17:02:09 -0600

    Adds update feature to trails and mountains

--- spec/features/mountains/index_spec.rb
+++ spec/features/mountains/index_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Mountains' do
+  before(:each) do
+    @mountain_1 = Mountain.create!(name: 'keystone', handicap_accessible: true, elevation: 5280)
+    @mountain_2 = Mountain.create!(name: 'breck peak 1', handicap_accessible: true, elevation: 5280)
+
+  end
+
   describe 'Mountains#index view' do
     it 'shows the name of each mountain' do
-      mountain_1 = Mountain.create!(name: 'keystone', handicap_accessible: true, elevation: 5280)
-      mountain_2 = Mountain.create!(name: 'breck peak 1', handicap_accessible: true, elevation: 5280)
       visit '/mountains'
-      expect(page).to have_content(mountain_1.name)
-      expect(page).to have_content(mountain_2.name)
+      expect(page).to have_content(@mountain_1.name)
+      expect(page).to have_content(@mountain_2.name)
 
     end
 
@@ -20,6 +24,14 @@ RSpec.describe 'Mountains' do
 
       expect(current_path).to eq("/mountains/new")
     end
+
+    it 'has a link to update the mountain' do
+      visit '/mountains'
+      expect(page).to have_link("Update #{@mountain_1.name}")
+      click_link("Update #{@mountain_1.name}")
+      expect(current_path).to eq("/mountains/#{@mountain_1.id}/edit")
+    end
+
 
   end
 end
--- spec/features/trails/index_spec.rb
+++ spec/features/trails/index_spec.rb
@@ -2,16 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Mountains' do
   describe 'Trails#index view' do
+    before(:each) do
+      @mountain_1 = Mountain.create!(name: 'keystone', handicap_accessible: true, elevation: 5280)
+      @trail_1 = Trail.create!(name: 'bunny_run', trail_open: true, mountain_id: @mountain_1.id, elevation_drop: 50)
+      @trail_2 = Trail.create!(name: 'schoolmarm', trail_open: true, mountain_id: @mountain_1.id, elevation_drop: 459)
+      @trail_3 = Trail.create!(name: 'zipper', trail_open: false, mountain_id: @mountain_1.id, elevation_drop: 600)
+
+    end
 
     it 'shows all the trail names' do
-      mountain_1 = Mountain.create!(name: 'keystone', handicap_accessible: true, elevation: 5280)
-      trail_1 = Trail.create!(name: 'bunny_run', trail_open: true, mountain_id: mountain_1.id, elevation_drop: 50)
-      trail_2 = Trail.create!(name: 'schoolmarm', trail_open: true, mountain_id: mountain_1.id, elevation_drop: 459)
-      trail_3 = Trail.create!(name: 'zipper', trail_open: false, mountain_id: mountain_1.id, elevation_drop: 600)
       visit "/trails"
-      expect(page).to have_content(trail_1.name)
-      expect(page).to have_content(trail_2.name)
-      expect(page).to_not have_content(trail_3.name)
+      expect(page).to have_content(@trail_1.name)
+      expect(page).to have_content(@trail_2.name)
+      expect(page).to_not have_content(@trail_3.name)
+    end
+
+    it 'has links to update the trail' do
+      visit "/trails"
+      expect(page).to have_content("Update #{@trail_1.name}")
+      click_link("Update #{@trail_1.name}")
+      expect(current_path).to eq("/trails/#{@trail_1.id}/edit")
     end
   end
 end
